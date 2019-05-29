87e44895c4f11cff144623ceba2a13e1f6f008fe
Droool <rjdeloss@gmail.com>
2019-05-29 14:06:10 -0400

    make it better

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -5,12 +5,11 @@ class Api::UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    if @user.save!
+    if @user.save
       login(@user)
-      render :index 
+      render json: @user
     else 
-      flash.now[:errors] = @user.errors.full_messages
-      render :new
+      render json:  @user.errors.full_messages, status: 401
     end
   end
 
