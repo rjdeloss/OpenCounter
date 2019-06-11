8be3aeac9e127bb1a901b6574307833b4c7fd5ed
Droool <rjdeloss@gmail.com>
2019-06-11 10:04:20 -0400

    added reviews and reservation controllers

---
+++ app/controllers/api/reservations_controller.rb
@@ -0,0 +1,27 @@
+class Api::ReservationsController < ApplicationController
+    before_action :ensure_logged_in
+
+    def show
+        @reservation = Reservation.find(params[:id])
+    end
+
+    def create
+        @reservation = Reservation.new(reservation_params)
+        @reservation.user_id = current_user.id
+        if @reservation.save!
+            render :show
+        else
+            render json: @reservation.errors.full_messages, status: 422
+        end
+    end
+
+    def destroy
+        @reservation = Reservation.find(params[:id])
+        @reservation.destroy
+        render :show
+    end
+    
+    def reservation_params
+        params.require(:reservation).permit(:start_datetime, :party_size)
+    end
+end
