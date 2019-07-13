4f56edd6f1b55457c177f4515c3debd98714cecb
Droool <rjdeloss@gmail.com>
2019-07-13 19:52:22 -0400

    refactored search form design

--- frontend/components/search/search.jsx
+++ frontend/components/search/search.jsx
@@ -22,6 +22,43 @@ const Search = (props) => {
     if (props.location.pathname === "/") {
         return (
             <form className="reservation-form" >
+                <section className="reservation-inputs">
+                    <div className="reservation-input-container">
+                        <div className="reservation-form-icon-decoration">
+                            <i className="material-icons">calendar_today</i>
+                        </div>
+                        <input className="reservation-form-input-field" type="date" />
+                    </div>
+                    <div className="reservation-input-container">
+                        <div className="reservation-form-icon-decoration">
+                            <i className="material-icons">access_time</i>
+                        </div>
+                        <select className="reservation-form-input-field" >
+                            <option value="7:00 PM">7:00 PM</option>
+                            <option value="8:00 PM">8:00 PM</option>
+                            <option value="9:00 PM">9:00 PM</option>
+                            <option value="10:00 PM">10:00 PM</option>
+                            <option value="11:00 PM">11:00 PM</option>
+                        </select>
+                    </div>
+                    <div className="reservation-input-container">
+                        <div className="reservation-form-icon-decoration">
+                            <i className="material-icons">person_outlined</i>
+                        </div>
+                        <select className="reservation-form-input-field">
+                            <option value="1">1 person</option>
+                            <option value="2">2 person</option>
+                            <option value="3">3 people</option>
+                            <option value="4">4 people</option>
+                            <option value="5">5 people</option>
+                            <option value="6">6 people</option>
+                            <option value="7">7 people</option>
+                            <option value="8">8 people</option>
+                            <option value="9">9 people</option>
+                            <option value="10">10 people</option>
+                        </select>
+                    </div>         
+                </section>
                 <div className="reservation-search-input">
                     <div className="reservation-form-icon-decoration">
                         <i className="material-icons">search</i>
@@ -36,13 +73,42 @@ const Search = (props) => {
             <>
                 <div className="compact-search-reservation-background">
                     <form className="compact-search-reservation-form">
-                        <div className="reservation-search-input">
-                            <div className="reservation-form-icon-decoration">
-                                <i className="material-icons">search</i>
+                        <section className="compact-reservation-inputs">
+                            <div className="compact-reservation-input-container">
+                                <select className="reservation-form-input-field">
+                                    <option value="1">1 person</option>
+                                    <option value="2">2 person</option>
+                                    <option value="3">3 people</option>
+                                    <option value="4">4 people</option>
+                                    <option value="5">5 people</option>
+                                    <option value="6">6 people</option>
+                                    <option value="7">7 people</option>
+                                    <option value="8">8 people</option>
+                                    <option value="9">9 people</option>
+                                    <option value="10">10 people</option>
+                                </select>
                             </div>
-                            <input type="text" value={searchValue} onChange={handleInput} placeholder="Manhattan" />
+                            <div className="compact-reservation-input-container">
+                                <input className="reservation-form-input-field" type="date" />
+                            </div>
+                            <div className="compact-reservation-input-container">
+                                <select className="reservation-form-input-field" >
+                                    <option value="7:00 PM">7:00 PM</option>
+                                    <option value="8:00 PM">8:00 PM</option>
+                                    <option value="9:00 PM">9:00 PM</option>
+                                    <option value="10:00 PM">10:00 PM</option>
+                                    <option value="11:00 PM">11:00 PM</option>
+                                </select>
+                            </div>
+                            
+                        <div className="compact-reservation-search-input">
+                            <div className="reservation-form-icon-decoration">
+                                <i className="material-icons grey">search</i>
+                            </div>
+                            <input type="text" value={searchValue} onChange={handleInput} placeholder="Location, Restaurant, or Cuisine" />
                         </div>
-                        <input className="reservation-submit-input" onClick={handleSubmit} type="submit" value="Let's go" />
+                        <input className="reservation-submit-input" onClick={handleSubmit} type="submit" value="Find a table" />
+                        </section>
                     </form>
                 </div>
                 <div className="search-result-container">
