ccfec72977fc70944e1d707059b7cd0d6d3802eb
Droool <rjdeloss@gmail.com>
2019-07-24 19:41:27 -0400

    created reservation logic in reservation model

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -17,8 +17,49 @@ class Reservation < ApplicationRecord
     belongs_to :user
     belongs_to :restaurant
 
+    before_valication :include_reservation_end_time
+
     def parsed_start_datetime 
         start_datetime.strftime("%a, %b %-d at %-l:%M %P")
+    end
+
+    def reset_timezone
+        start_datetime.change(offset: "+0000")
+    end
+
+    def include_reservation_end_time
+        self.end_datetime = self.start_datetime + 1.hour
+    end
+    
+    def operating_hours
+        unless (start_datetime <= (restaurant.close_time - 1.hour).change(
+            day: start_datetime.day, 
+            month: start_datetime.month, 
+            year: start_datetime.year, 
+            )) && (
+                start_datetime >= restaurant.open_time.change(
+                    day: start_datetime.day, 
+                    month: start_datetime.month, 
+                    year: start_datetime.year, 
+                    ))
+                    errors[:reservation] << "Restaurant must be open to make reservation"
+                end
+            end
+
+    def booking_reservations 
+        if start_datetime < DateTime.now.change(offset: "+0000")
+            errors[:reservation] << "Cannot make reservations before current time"
+        end
+    end
+    
+    def no_customer_overlap
+        start_time = self.start_datetime - 1.hour
+        end_time = self.end_datetime + 1.hour
+
+        if Reservation.where("start_datetime > ? AND end_datetime < ?", start_time, end_time)
+            .where("user_id = ?", user_id).exsists?
+            errors[:reservation] << "We're sorry. Reservation has been taken"
+        end
     end
 
 end
--- db/migrate/20190604132940_create_reservations.rb
+++ db/migrate/20190604132940_create_reservations.rb
@@ -4,6 +4,7 @@ class CreateReservations < ActiveRecord::Migration[5.2]
       t.integer :user_id, null: false
       t.integer :restaurant_id, null: false
       t.datetime :start_datetime, null: false
+      t.datetime :end_datetime
       t.integer :party_size, null: false
 
       t.timestamps
