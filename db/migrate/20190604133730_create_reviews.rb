ac39e605fec07debf51a8cc23a8fb6795c6aeae5
Droool <rjdeloss@gmail.com>
2019-07-31 16:50:02 -0400

    added review logic on the backend

--- db/migrate/20190604133730_create_reviews.rb
+++ db/migrate/20190604133730_create_reviews.rb
@@ -1,12 +1,15 @@
 class CreateReviews < ActiveRecord::Migration[5.2]
   def change
     create_table :reviews do |t|
-      t.integer :user_id
+      t.integer :user_id 
       t.integer :restaurant_id
       t.integer :overall_rating
       t.integer :food_rating
+      t.integer :service_rating
       t.integer :ambiance_rating
       t.integer :value_rating
+      t.integer :noise_level
+      t.integer :recommended, default: 0
       t.text :body
 
       t.timestamps
