778e6c1dee4c69229b89c8d507970a61efc8d22b
Droool <rjdeloss@gmail.com>
2019-07-31 13:27:06 -0400

    added reservation_confirmation to slice

--- frontend/components/reservation_form/reservation_form_container.js
+++ frontend/components/reservation_form/reservation_form_container.js
@@ -2,20 +2,23 @@ import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 import ReservationForm from './reservation_form';
 import { openModal } from '../../actions/modal_action';
-import { createReservation, deleteReservation } from '../../actions/reservation_action';
+import { createReservation, deleteReservation, clearReservationConfirmation } from '../../actions/reservation_action';
 
 const msp = (state, ownProps) => {
+    debugger
     return ({
         restaurant: state.entities.restaurants[ownProps.match.params.restaurantId],
         userId: state.session.currentUser,
         errors: state.errors.reservation, 
         reservation: state.entities.reservation, 
+        confirmation: state.ui.reservation.confirmation
     })
 }
 
 const mdp = dispatch => ({
     createReservation: reservation => dispatch(createReservation(reservation)), 
     deleteReservation: id => dispatch(deleteReservation(id)), 
+    clearReservationConfirmation: () => dispatch(clearReservationConfirmation()),
     openLogin: () => dispatch(openModal("signin"))
 });
 
