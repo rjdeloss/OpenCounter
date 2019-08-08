6b3f1880b9dd062f03b1faa70c5aa15d9b963c4b
Droool <rjdeloss@gmail.com>
2019-08-07 23:21:06 -0400

    Edit review form

--- frontend/components/reviews/reviews_form.jsx
+++ frontend/components/reviews/reviews_form.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import Rating from 'react-rating';
 import moment from 'moment';
 
 class ReviewsForm extends React.Component {
     constructor(props){
         super(props); 
+        debugger
+        this.state = {
+            food_rating: 0,
+            service_rating: 0,
+            ambiance_rating: 0, 
+            value_rating: 0,
+            noise_level: 0,
+        }
     }    
     
     componentWillUnmount(){
@@ -39,9 +48,62 @@ class ReviewsForm extends React.Component {
     }
 
     render() {
+        
         return (
-            <div className="reviews-form">
-                <h1>This is a review!</h1>
+            <div className="reviews-form-container">
+                <h2>Name, how was your experience at The Cecil Steakhouse? </h2>
+                <span>Dining Date</span>
+                <form className="reviews-form" action="">
+                    <h3 className="bold-text center-text">Rate your dining experience (required)</h3>
+                    <label htmlFor="" className="reviews-form-input-container">Food
+                        <div className="reviews-radio-collection">
+                            <Rating
+                                emptySymbol={<i className="material-icons grey star-rating">star_outlined</i>}
+                                fullSymbol={<i className="material-icons red">star</i>}
+                            />
+                        </div>
+                    </label>
+                    <label htmlFor="" className="reviews-form-input-container">Service
+                        <div className="reviews-radio-collection">
+                            <Rating
+                                emptySymbol={<i className="material-icons grey star-rating">star_outlined</i>}
+                                fullSymbol={<i className="material-icons red">star</i>}
+                            />
+                        </div>
+                    </label>
+                    <label htmlFor="" className="reviews-form-input-container">Ambience
+                        <div className="reviews-radio-collection">
+                            <Rating
+                                emptySymbol={<i className="material-icons grey star-rating">star_outlined</i>}
+                                fullSymbol={<i className="material-icons red">star</i>}
+                            />
+                        </div>
+                    </label>
+                    <label htmlFor="" className="reviews-form-input-container">Value
+                        <div className="reviews-radio-collection">
+                            <Rating
+                                emptySymbol={<i className="material-icons grey star-rating">star_outlined</i>}
+                                fullSymbol={<i className="material-icons red">star</i>}
+                            />
+                        </div>
+                    </label>
+                    <label htmlFor="" className="reviews-form-input-container">Noise Level
+                        <div className="reviews-radio-collection">
+                            <input type="radio" name="noise" id="" value="1" />
+                            <input type="radio" name="noise" id="" value="2" />
+                            <input type="radio" name="noise" id="" value="3" />
+                        </div>
+                    </label>
+                    <h3 className="bold-text center-text">Would you recommend (The Cecil Steakhouse) to a friend?</h3>
+                        <div className="reviews-form-input-container">
+                            <input type="radio" name="noise" id="" value="1" />Yes
+                            <input type="radio" name="noise" id="" value="1" />No
+                        </div>
+                    <h3 className="bold-text center-text">Write a review</h3>
+                        <input type="textarea" name="" id=""/>
+
+                    <input type="submit" value="Submit your Review" name="" id=""/>
+                </form>
             </div>
         )
     }
