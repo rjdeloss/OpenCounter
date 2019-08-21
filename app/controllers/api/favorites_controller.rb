3c7d94bfce84a2a04342ecca25ef8f570f71a043
Droool <rjdeloss@gmail.com>
2019-08-21 19:02:17 -0400

    create controller and view for favorites

---
+++ app/controllers/api/favorites_controller.rb
@@ -0,0 +1,26 @@
+class Api::FavoritesController < ApplicationController
+    before_action :require_logged_in
+
+    def index 
+        @favorites = Favorite.where('user_id = ?', params[:userId])
+    end
+    
+    def create
+        @favoreite = Favorite.new(favorite_params)
+        if @favorite.save
+            render :show
+        else
+            render json: @favorite.errors.full_messages, status: 422
+        end
+    end
+
+    def destroy 
+        @favorite = Favorite.find(params[:id])
+        @favorite.destroy
+        render :show
+    end
+
+    def favorite_params
+        params.require(:favorite).permit(:user_id, :restaurant_id)
+    end
+end
