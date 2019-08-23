38a0045daed5a1d1ce3aa4ea0187917013d05fb7
Droool <rjdeloss@gmail.com>
2019-08-23 17:39:38 -0400

    add empty object fix for favorites and reviews

--- app/views/api/users/_user.json.jbuilder
+++ app/views/api/users/_user.json.jbuilder
@@ -1,14 +1,13 @@
 json.extract! user, :id, :fname, :lname, :email
 
 json.favorites do 
+    debugger
     user.favorites.each do |favorite|
+        debugger
         json.set! favorite.id do 
             json.partial! 'api/favorites/favorite.json.jbuilder', favorite: favorite
             json.overall_rating favorite.restaurant.get_all_ratings
-            # json.id favorite.id
-            # json.user_id favorite.user_id
-            # json.restaurant_id favorite.restaurant_id
-            # json.restaurant favorite.restaurant
         end
     end 
 end
+json.favorites({}) if user.favorites.empty?
--- frontend/components/favorites/favorites.jsx
+++ frontend/components/favorites/favorites.jsx
@@ -1,37 +1,36 @@
 import React from 'react';
 
 const FavoriteButton = props => {
-    debugger
+    // debugger
     const currentUser = props.currentUser; 
     const restaurant = props.restaurant;
     const favorites = props.favorites;
     let createFavorite = props.createFavorite;
 
-    let favorite = {
-        user_id: currentUser,
-        restaurant_id: restaurant.id, 
-    }
+    // let favoredRestaurants = favorites.map(favorite => favorite.restaurant_id);
+    // let favorite = {
+    //     user_id: currentUser,
+    //     restaurant_id: restaurant.id, 
+    // }
 
-    let renderFavoriteButton = () => {
-        if (props.loggedIn) {
-            
-        }
+    // let favoriteButton = 
+    //     !favoredRestaurants.includes(restaurant.id) ? 
+    //         <a href="" className="favorite-button" onClick={(e) => {e.preventDefault(); createFavorite(favorite)}}>
+    //             <i className="material-icons">bookmark_border</i>
+    //             <p>Save this restaurant</p>
+    //         </a> :
+    //         <a href="" className="favorite-button" onClick={(e) => { e.preventDefault(); deleteFavorite() }}>
+    //             <i className="material-icons red">bookmark</i>
+    //             <p>Restaurant saved!</p>
+    //         </a>;
 
-    }
-        // props.loggedIn ? 
-        // !(props.favorites.includes(restaurant.id)) ?
-        // <a href="" className="favorite-button" onClick={(e) => {e.preventDefault(); createFavorite(favorite)}}>
-        //     <i className="material-icons">bookmark_border</i>
-        //     <p>Save this restaurant</p>
-        // </a> : 
-        // <a href="" className="favorite-button" onClick={(e) => { e.preventDefault(); deleteFavorite(favorite) }}>
-        //     <i className="material-icons red">bookmark</i>
-        //     <p>Restaurant saved!</p>
-        // </a> : null;
+
+    // let renderFavoriteButton = 
+    //     (props.loggedIn) ? favoriteButton : null;
 
     return (
         <>
-            {renderFavoriteButton}
+            {/* {renderFavoriteButton} */}
         </>
     )
 }
