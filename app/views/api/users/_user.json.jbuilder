e8ca7f6826982606d6e6191d3da2023a6579c92f
Droool <rjdeloss@gmail.com>
2019-08-23 16:16:21 -0400

    modify favorites jbuilder

--- app/views/api/users/_user.json.jbuilder
+++ app/views/api/users/_user.json.jbuilder
@@ -1,1 +1,14 @@
 json.extract! user, :id, :fname, :lname, :email
+
+json.favorites do 
+    user.favorites.each do |favorite|
+        json.set! favorite.id do 
+            json.partial! 'api/favorites/favorite.json.jbuilder', favorite: favorite
+            json.overall_rating favorite.restaurant.get_all_ratings
+            # json.id favorite.id
+            # json.user_id favorite.user_id
+            # json.restaurant_id favorite.restaurant_id
+            # json.restaurant favorite.restaurant
+        end
+    end 
+end
--- frontend/reducers/users_reducer.js
+++ frontend/reducers/users_reducer.js
@@ -1,12 +1,25 @@
-import { RECEIVE_CURRENT_USER } from '../actions/session_action';
+import { RECEIVE_CURRENT_USER, LOGOUT_CURRENT_USER } from '../actions/session_action';
+import { RECEIVE_FAVORITE, REMOVE_FAVORITE } from '../actions/favorite_action';
 import { merge } from 'lodash';
-
 
 export const usersReducer = (state = {}, action) => {
     Object.freeze(state);
     switch (action.type) {
         case RECEIVE_CURRENT_USER:
-            return merge({}, state, { [action.currentUser.id]: action.currentUser });
+            return merge({}, state, { [action.currentUser.id]: action.currentUser,  });
+        case RECEIVE_FAVORITE:
+            debugger
+            let newState = merge({}, state);
+            let favorite = action.favorite; 
+            newState[favorite.user_id].favorites[favorite.id] = favorite;
+            return newState
+        case REMOVE_FAVORITE:
+            newState = merge({}, state);
+            favorite = action.id.favorite; 
+            delete newState[favorite.user_id].favorites[favorite.id];
+            return newState;
+        case LOGOUT_CURRENT_USER:
+            return {}
         default:
             return state;
     }
