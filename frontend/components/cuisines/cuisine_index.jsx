ec08adcdb3cb5d0c3441c282cb5a125b3670fd7b
Droool <rjdeloss@gmail.com>
2019-08-26 18:25:46 -0400

    add background images to cuisines

--- frontend/components/cuisines/cuisine_index.jsx
+++ frontend/components/cuisines/cuisine_index.jsx
@@ -3,9 +3,22 @@ import React, { useEffect } from 'react';
 import CuisineIndexItem from './cuisine_index_item';
 
 const CuisineIndex = (props) => {
+    const backgrounds = [
+        "cuisine-american",
+        "cuisine-chinese",
+        "cuisine-french",
+        "cuisine-indian",
+        "cuisine-italian",
+        "cuisine-japanese",
+        "cuisine-korean",
+        "cuisine-latin",
+        "cuisine-mexican",
+        "cuisine-thai",
+        "cuisine-vietnamese"
+    ]
     
-    const cuisines = props.cuisines.map(cuisine => (
-        <CuisineIndexItem key={cuisine.id} cuisine={cuisine} searchRestaurants={props.searchRestaurants} />
+    const cuisines = props.cuisines.map((cuisine, i) => (
+        <CuisineIndexItem key={cuisine.id} cuisine={cuisine} searchRestaurants={props.searchRestaurants} backgrounds={backgrounds[i]}/>
     ));
 
     useEffect( () => { props.fetchCuisines(); }, []);
