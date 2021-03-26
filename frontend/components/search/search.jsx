a1155195e047f50fe64659d0c40dcf1f63768320
Rafiluis De Los Santos <>
2021-03-26 15:02:10 -0400

    modify reservation form on search page

--- frontend/components/search/search.jsx
+++ frontend/components/search/search.jsx
@@ -72,44 +72,54 @@ const Search = (props) => {
         return (
             <>
                 <div className="compact-search-reservation-background">
-                    <form className="compact-search-reservation-form">
-                        <section className="compact-reservation-inputs">
-                            <div className="compact-reservation-input-container">
-                                <select className="reservation-form-input-field">
-                                    <option value="1">1 person</option>
-                                    <option value="2">2 person</option>
-                                    <option value="3">3 people</option>
-                                    <option value="4">4 people</option>
-                                    <option value="5">5 people</option>
-                                    <option value="6">6 people</option>
-                                    <option value="7">7 people</option>
-                                    <option value="8">8 people</option>
-                                    <option value="9">9 people</option>
-                                    <option value="10">10 people</option>
-                                </select>
-                            </div>
-                            <div className="compact-reservation-input-container">
-                                <input className="reservation-form-input-field" type="date" />
-                            </div>
-                            <div className="compact-reservation-input-container">
-                                <select className="reservation-form-input-field" >
-                                    <option value="7:00 PM">7:00 PM</option>
-                                    <option value="8:00 PM">8:00 PM</option>
-                                    <option value="9:00 PM">9:00 PM</option>
-                                    <option value="10:00 PM">10:00 PM</option>
-                                    <option value="11:00 PM">11:00 PM</option>
-                                </select>
-                            </div>
-                            
-                        <div className="compact-reservation-search-input">
+                    <form className="reservation-form" >
+                <section className="reservation-inputs-container">
+                    <div className="reservation-multi-search">
+                        <div className="reservation-input reservation-input-right">
                             <div className="reservation-form-icon-decoration">
-                                <i className="material-icons grey">search</i>
+                                <i className="material-icons">calendar_today</i>
                             </div>
-                            <input type="text" value={searchValue} onChange={handleInput} placeholder="Location, Restaurant, or Cuisine" />
+                            <input className="reservation-form-input-field" type="date" />
                         </div>
-                        <input className="reservation-submit-input" onClick={handleSubmit} type="submit" value="Find a table" />
-                        </section>
-                    </form>
+                        <div className="reservation-input reservation-input-left">
+                            <div className="reservation-form-icon-decoration">
+                                <i className="material-icons">access_time</i>
+                            </div>
+                            <select className="reservation-form-input-field" >
+                                <option value="7:00 PM">7:00 PM</option>
+                                <option value="8:00 PM">8:00 PM</option>
+                                <option value="9:00 PM">9:00 PM</option>
+                                <option value="10:00 PM">10:00 PM</option>
+                                <option value="11:00 PM">11:00 PM</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div className="reservation-input reservation-input-party">
+                        <div className="reservation-form-icon-decoration">
+                            <i className="material-icons">perm_identity</i>
+                        </div>
+                        <select className="reservation-form-input-field">
+                            <option value="1">1 person</option>
+                            <option value="2">2 person</option>
+                            <option value="3">3 people</option>
+                            <option value="4">4 people</option>
+                            <option value="5">5 people</option>
+                            <option value="6">6 people</option>
+                            <option value="7">7 people</option>
+                            <option value="8">8 people</option>
+                            <option value="9">9 people</option>
+                            <option value="10">10 people</option>
+                        </select>
+                    </div>         
+                </section>
+                <div className="reservation-input reservation-input-search">
+                    <div className="reservation-form-icon-decoration">
+                        <i className="material-icons">search</i>
+                    </div>
+                    <input className="reservation-form-search-field" type="text" value={searchValue} onChange={handleInput} placeholder="Manhattan" />
+                </div>
+                <input className="reservation-submit-input" onClick={handleSubmit} type="submit" value="Find a Table" />
+            </form>
                 </div>
                 <div className="search-result-container">
                     <RestaurantsIndex />
